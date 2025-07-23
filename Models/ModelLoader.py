2217bb3d396071c2be185fdc14c26c8a021b1503
bedirhangenc1aslan <bedirhangenc1aslan@gmail.com>
2025-07-23 09:29:32 +0300

    fix : Matcher

--- Models/ModelLoader.py
+++ Models/ModelLoader.py
@@ -1,16 +1,17 @@
 from .BoxPredictor import BoxPredictor
 from .Matcher import Matcher
 import torch
+from pathlib import Path
+
 class ModelLoader:
     def __init__(self):
         self.model_dict = {}
-
     def load_model(self , MODEL_PATH , TYPE , DEVICE):
         if TYPE == "BoxPredictor":
             model = BoxPredictor()
             model.load_state_dict(torch.load(MODEL_PATH, map_location=DEVICE))
         elif TYPE == "Matcher":
-            model = Matcher()
+            model = Matcher(16 , 2)
             model.load_state_dict(torch.load(MODEL_PATH, map_location=DEVICE))
         self.model_dict[MODEL_PATH] = model
         return model
